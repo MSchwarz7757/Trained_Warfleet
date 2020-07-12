898c8ab0df0991a9436a4974ec01759bb6ee96cb
M.Schwarz <Michel_Schwarz@ymail.com>
2020-07-12 11:57:35 +0200

    changed console outputx

--- Warfleet_Gym_AI/testAgents.py
+++ Warfleet_Gym_AI/testAgents.py
@@ -7,12 +7,12 @@ from stable_baselines import PPO2, A2C
 
 from mlp import Policy
 
-#from Warfleet_Gym_AI.gym_wf.envs import wf_env as e
+from gym_wf.envs.wf_env import CustomEnv
 
 
 def main():
     alg_input = input("Select algorithm (PPO2 or A2C only):")
-    if alg_input != "PPO2" and alg_input != "A2C":
+    if alg_input != "PPO2" and alg_input != "A2C" and alg_input != "ppo2" and alg_input != "a2c":
         print("Not an option (PPO2 or A2C only) !")
         alg_input = input("Select algorithm (PPO2 or A2C only):")
     model_input = "trained_agents\\" + input("Select model to test(input filename, eg. a2c_wf_2):")
@@ -26,31 +26,22 @@ def main():
     stage_reward = 0
     turns = 0
 
-    if alg_input == "PPO2":
+    if alg_input == "PPO2" or alg_input == "ppo2":
         model = PPO2.load(model_input, env=env, tensorboard_log=log_dir)
-    elif alg_input == "A2C":
+    elif alg_input == "A2C" or alg_input == "a2c":
         model = A2C.load(model_input, env=env, tensorboard_log=log_dir)
 
     obs = env.reset()
 
     while not done:
-        # print("Game is running")
         action, _states = model.predict(obs)
-        # print("---------------action:", action.shape, action)
         obs, reward, done, info = env.step(action)
         stage_reward += reward
         turns = turns + 1
-        # print("Reward in ever round: {}".format(reward))
         # env.render()
-        # print("Trefferliste: {}".format(env.hitlist_agent))
 
     print("Reward: {} /42".format(stage_reward))
     print("Turns: {}".format(turns))
-
-    # insert into steps function in wf_env
-    # print("Remaining Ships Computer: {}".format(len(self.shiplist_enemy)))
-    # print("Remaining Ships Agent: {}".format(len(self.shiplist_agent)))
-
     env.close()
 
 
--- Warfleet_Gym_AI/trainAgents.py
+++ Warfleet_Gym_AI/trainAgents.py
@@ -10,24 +10,24 @@ from mlp import Policy
 
 
 def main():
+    # choose the algorithm which should be trained
+    alg_input = input("Select algorithm (PPO2 or A2C only): ")
+    timesteps_input = int(input("Choose number of timesteps: "))
+    model_save = "trained_agents\\" + input("Select model to test(input filename, eg. a2c_wf_2 or ppo2_wf_4):")
+
     # the enviroment is generated
     env = gym.make("WARFLEET-v0")
     # The algorithms require a vectorized environment to run
     env = DummyVecEnv([lambda: env])
     log_dir = "./logs/"
 
-    # choose the algorithm which should be trained
-    alg_input = input("Select algorithm (PPO2 or A2C only): ")
-    timesteps_input = int(input("Choose number of timesteps: "))
-
-    if alg_input == "A2C":
-        model_save = "trained_agents\\" + input("Select model to test(input filename, eg. a2c_wf_2):")
+    # start training by input
+    if alg_input == "A2C" or alg_input == "a2c":
         model = A2C(MlpPolicy, env, verbose=1, tensorboard_log=log_dir)
         model.learn(total_timesteps=timesteps_input)
         model.save(model_save)
     else:
-        if alg_input == "PPO2":
-            model_save = "trained_agents\\" + input("Select model to test(input filename, eg. ppo2_wf_4):")
+        if alg_input == "PPO2" or alg_input == "ppo2":
             model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log=log_dir, cliprange=0.1, gamma=0.99, ent_coef=0.001, vf_coef=0.2)
             model.learn(total_timesteps=timesteps_input)
             model.save(model_save)
