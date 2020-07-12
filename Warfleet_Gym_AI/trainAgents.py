d137ab32d2b96fbd4333c31b7bff4f9e72d4c90e
M.Schwarz <Michel_Schwarz@ymail.com>
2020-07-12 10:27:42 +0200

    changed file names

--- Warfleet_Gym_AI/trainAgents.py
+++ Warfleet_Gym_AI/trainAgents.py
@@ -16,19 +16,24 @@ def main():
     env = DummyVecEnv([lambda: env])
     log_dir = "./logs/"
 
+    # choose the algorithm which should be trained
     alg_input = input("Select algorithm (PPO2 or A2C only): ")
     timesteps_input = int(input("Choose number of timesteps: "))
+
     if alg_input == "A2C":
+        model_save = "trained_agents\\" + input("Select model to test(input filename, eg. a2c_wf_2):")
         model = A2C(MlpPolicy, env, verbose=1, tensorboard_log=log_dir)
         model.learn(total_timesteps=timesteps_input)
-        model.save("trained_agents/a2c_wf_4")
+        model.save(model_save)
     else:
         if alg_input == "PPO2":
+            model_save = "trained_agents\\" + input("Select model to test(input filename, eg. ppo2_wf_4):")
             model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log=log_dir, cliprange=0.1, gamma=0.99, ent_coef=0.001, vf_coef=0.2)
             model.learn(total_timesteps=timesteps_input)
-            model.save("trained_agents/ppo2_wf_4")
+            model.save(model_save)
         else:
             print("incorrect input")
+
 
 
 if __name__ == "__main__":
